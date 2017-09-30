5d0f94e4287e5d526d303e3949ab150385173d2a
jocelynharper <togepie@live.com>
2017-09-30 17:02:17 -0400

    Updating React-DOM and React-Bootstrap

--- src/Search.js
+++ src/Search.js
@@ -1,28 +1,35 @@
 import React, { Component } from 'react';
+import {FormControl, FormGroup} from 'react-boostrap';
+import ReactDOM from 'react-dom';
 
 class Search extends Component{
 
+  componentDidMount() {
+
+    const formInstance = (
+      <form>
+        <FormGroup bsSize="large">
+          <FormControl type="text" placeholder="Large text" />
+        </FormGroup>
+        <FormGroup>
+          <FormControl type="text" placeholder="Normal text" />
+        </FormGroup>
+        <FormGroup bsSize="small">
+          <FormControl type="text" placeholder="Small text" />
+        </FormGroup>
+      </form>
+    );
+
+    ReactDOM.render(formInstance, mountNode);
+
+  }
+
 render() {
 
-return(
-
-const formInstance = (
-  <form>
-    <FormGroup bsSize="large">
-      <FormControl type="text" placeholder="Large text" />
-    </FormGroup>
-    <FormGroup>
-      <FormControl type="text" placeholder="Normal text" />
-    </FormGroup>
-    <FormGroup bsSize="small">
-      <FormControl type="text" placeholder="Small text" />
-    </FormGroup>
-  </form>
-);
-
-ReactDOM.render(formInstance, mountNode);
-
-)}
+  return(
+    <formInstance></formInstance>
+  )
+  }
 }
 
-export default Search
+export default Search;
