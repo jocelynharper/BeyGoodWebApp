88de850c3be4fe8b46dcc079e3574109357a36de
jocelynharper <togepie@live.com>
2017-10-01 18:23:00 -0400

    Adding Sinon and updating React-Native

--- src/Footer.test.js
+++ src/Footer.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Footer from './Footer.js'
 
-test('SearchBar component should render as expected', () => {
+test('Footer component should render with GIPHY img/link in the bottom, right corner', () => {
   const component = shallow(<Footer />)
-  expect(component.contains('Testing a test lol').toBe(true))
+  it(component.contains("https://i.imgur.com/9ML17bb.png").toBe(true))
 })
