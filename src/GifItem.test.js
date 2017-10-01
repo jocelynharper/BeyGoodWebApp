54a149c005e97d4b0fa58de97256e566fd1d482d
jocelynharper <togepie@live.com>
2017-10-01 18:54:11 -0400

    Tests

--- src/GifItem.test.js
+++ src/GifItem.test.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Gifitem from './Gifitem.js'
 
-test('SearchBar component should render as expected', () => {
-  const component = shallow(<Gifitem />)
-  expect(component.contains('Testing a test lol').toBe(true))
-})
+const result = ('<GifItem />', () => {
+it('Renders 1 GifItem component', () => {
+  const component = shallow(<GifItem />);
+  expect(component).toHaveLength(1);
+  });
+});
--- src/SearchBar.test.js
+++ src/SearchBar.test.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import SearchBar from './SearchBar.js'
 
-test('works', () => {
-  const component = shallow(<SearchBar />)
-  expect(component(1).toql(2))
-})
+describe('<SearchBar />', () => {
+it('Renders 1 SearchBar component', () => {
+  const component = shallow(<SearchBar />);
+  expect(component).toHaveLength(1);
+  });
+});
