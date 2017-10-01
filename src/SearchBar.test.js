f4929fb102be0abfa650ff2ef10d3fee4617239e
jocelynharper <togepie@live.com>
2017-10-01 18:29:41 -0400

    Search Bar test

--- src/SearchBar.test.js
+++ src/SearchBar.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import SearchBar from './SearchBar.js'
 
-test('SearchBar component should render as expected', () => {
+test('works', () => {
   const component = shallow(<SearchBar />)
-  expect(component.contains('Testing a test lol').toBe(true))
+  expect(component(1).toql(2))
 })
